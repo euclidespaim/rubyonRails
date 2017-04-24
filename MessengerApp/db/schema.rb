2312f7c25b6117dccae3c9948af21fff99bf3bb0
euclidespaim <euclidespaim@gmail.com>
2017-04-24 16:49:55 -0300

    Rails app many-to-many data model!

--- MessengerApp/db/schema.rb
+++ MessengerApp/db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420111909) do
+ActiveRecord::Schema.define(version: 20170424172541) do
+
+  create_table "actors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "parts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
